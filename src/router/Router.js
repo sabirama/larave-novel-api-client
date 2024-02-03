4fbe723b0dcad17f193a7e05651573e188d14960
sabirama <markcondes21@gmail.com>
2024-02-03 21:14:24 +0800

    await from main removed

--- src/router/Router.js
+++ src/router/Router.js
@@ -31,7 +31,7 @@ const createRoutes = async () => {
 
 const MainRouter = createRouter({
     history: createWebHistory(),
-    routes: await createRoutes()
+    routes: createRoutes()
 });
 
 export default MainRouter;
